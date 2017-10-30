be0a5d2dc84103a5c5e49d278ddc2608fa7982d6
Xueyin Wan <Xueyin.Wan@microsoft.com>
2017-10-29 20:31:55 -0700

    add extensibility

--- CSharpIntermediate/Section5/Extensibility/Extensibility/FileLogger.cs
+++ CSharpIntermediate/Section5/Extensibility/Extensibility/FileLogger.cs
@@ -23,7 +23,7 @@ namespace Extensibility
 
         private void Log(string message, string messageType)
         {
-            using (var streamWriter = new StreamWriter(_path, true))
+            using (var streamWriter = new StreamWriter(_path, true)) // streanWriter.Dispose(); 当exception发生的时候，会自动call这一句，using里面自动帮我们call了这一句!!!
             {
                 streamWriter.WriteLine(messageType + ": " + message);
             }
